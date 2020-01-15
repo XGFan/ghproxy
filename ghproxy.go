8eb7de580c2810d8c6a9720855c393a81475f4c6
Guofan Xu <xuguofan@live.com>
2020-01-15 13:22:46 +0800

    init
    Signed-off-by: Guofan Xu <xuguofan@live.com>

---
+++ ghproxy.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/*", proxy)
+	e.GET("/", func(context echo.Context) error {
+		return context.String(200, "It works")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func proxy(c echo.Context) error {
+	realDownloadUrl := "https://github.com/" + c.Request().URL.String()
+	resp, _ := http.DefaultClient.Get(realDownloadUrl)
+	c.Response().Header().Add("Content-Disposition", resp.Header.Get("Content-Disposition"))
+	return c.Stream(200, resp.Header.Get("Content-Type"), resp.Body)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module ghproxy
+
+go 1.13
+
+require github.com/labstack/echo/v4 v4.1.13
