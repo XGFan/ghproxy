204bf84cfd3d40e0d2fe06571995267c1e5758c9
Magic Kevin <33132228+KevinZonda@users.noreply.github.com>
2020-06-02 23:35:28 +0800

    Try to fix tcp mem Leak #2

--- ghproxy.go
+++ ghproxy.go
@@ -17,6 +17,7 @@ func main() {
 func proxy(c echo.Context) error {
 	realDownloadUrl := "https://github.com/" + c.Request().URL.String()
 	resp, _ := http.DefaultClient.Get(realDownloadUrl)
+	defer resp.Body.Close()
 	c.Response().Header().Add("Content-Disposition", resp.Header.Get("Content-Disposition"))
 	return c.Stream(200, resp.Header.Get("Content-Type"), resp.Body)
 }
